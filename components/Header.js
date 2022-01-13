dac4ad4738f7526d17642366bc4bf7b118ca4bce
hricha05 <harun.richards@outlook.com>
2022-01-13 18:14:37 +0000

    commit

--- components/Header.js
+++ components/Header.js
@@ -1,3 +1,10 @@
-export default function Header({ title }) {
-  return <h1 className="title">{title}</h1>
+export default function Header({ tagline }) {
+	return (
+		<div>
+			<h1 className="title">
+				Hi, I'm <span className="title-name">Harun Richards</span>
+			</h1>
+			<h2 className="tag-line">{tagline}</h2>
+		</div>
+	);
 }
--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,27 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import Head from 'next/head';
+import Header from '@components/Header';
+import Footer from '@components/Footer';
 
 export default function Home() {
-  return (
-    <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+	return (
+		<div className="container">
+			<Head>
+				<title>Harun Richards</title>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
 
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
+			<main>
+				<Header tagline="A Frontend Web Developer" />
+				<p className="description">
+					Proactive, multi-skilled and a highly resourceful individual with a few
+					years experience manipulating data. High proficiency in several programming
+					principles and languages. Passionate about improving things and carry this
+					out through analysis and problem solving.Always on the look out for a more
+					efficient way of doing things.
+				</p>
+			</main>
 
-      <Footer />
-    </div>
-  )
+			<Footer />
+		</div>
+	);
 }
